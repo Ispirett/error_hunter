0d346ed50c24d03ce94e8ef34ea339c7eb261ab9
ispirett <ispirett@gmail.com>
2020-03-24 01:21:27 -0400

    refactor(app_error): rename serverity to severity

--- db/migrate/20200324050855_rename_column_serverity_from_app_errors.rb
+++ db/migrate/20200324050855_rename_column_serverity_from_app_errors.rb
@@ -1,4 +1,4 @@
-class RenameColumnseverityFromAppErrors < ActiveRecord::Migration[6.0]
+class RenameColumnServerityFromAppErrors < ActiveRecord::Migration[6.0]
   def change
     rename_column :app_errors, :serverity, :severity
   end
